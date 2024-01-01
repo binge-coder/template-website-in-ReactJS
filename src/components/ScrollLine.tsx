d831c8ffbde02554e6aa9b84bfced40e179c1a4b
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2024-01-02 00:31:56 +0530

    better scrollLine color

--- src/components/ScrollLine.tsx
+++ src/components/ScrollLine.tsx
@@ -8,7 +8,7 @@ const ScrollLine = () => {
       restDelta: 0.001
     });
   return (
-    <motion.div className="sticky bg-secondary top-0 left-0 right-0 h-2 origin-left" style={{ scaleX }} />
+    <motion.div className="sticky bg-accent top-0 left-0 right-0 h-2 origin-left" style={{ scaleX }} />
   )
 }
 
